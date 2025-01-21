41332bb98615d29d521b7d976f3d5e071b554083
dearguru07 <pangalaguruyadav101@gmail.com>
2025-01-21 15:30:06 +0530

    koll

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ let x = {
 if (x == 1 && x == 2 && x == 3) {
   console.log("Hello..");
 }
+
+
+let u = {
+    value: 1,valueOf: function () {
+      returnthis.value++;
+    },
+  };
+  if (x == 1 && x == 2 && x == 3) {
+    console.log("Hello..");
+  }
+  
