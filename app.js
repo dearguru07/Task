c0117af25291b7d95b52c63fad0bf7b844722e95
dearguru07 <pangalaguruyadav101@gmail.com>
2025-01-21 15:23:19 +0530

    kol

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-let x
-if(x==1 && x==2 && x==3){
-    console.log('Hello..')
+let x = {
+  value: 1,valueOf: function () {
+    returnthis.value++;
+  },
+};
+if (x == 1 && x == 2 && x == 3) {
+  console.log("Hello..");
 }
